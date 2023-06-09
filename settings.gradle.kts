7ba1333b05f88974569508acc1297adc57558fff
Tobias Watzl <twatzl@gmx.at>
2023-06-09 23:08:26 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+include("museum-railway-eventcollectors")
+
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+rootProject.name = "museum-railway-events"
