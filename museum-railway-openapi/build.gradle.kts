48dd9437ac95f3c6e134327330e0a379ec08887e
Tobias Watzl <twatzl@gmx.at>
2024-01-18 22:50:04 +0100

    correctly generate museum railway api client

--- museum-railway-openapi/build.gradle.kts
+++ museum-railway-openapi/build.gradle.kts
@@ -10,24 +10,25 @@ buildscript {
 }
 
 plugins {
-    id("org.openapi.generator") version "7.0.1"
+    id("org.openapi.generator") version "7.2.0"
 }
 
 group = "at.museumsbahnen"
-version = "0.0.1"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
 }
 
 openApiGenerate {
-    generatorName.set("typescript-axios")
     inputSpec.set("${project.projectDir}/src/main/resources/swagger.yaml")
-    outputDir.set("${project.projectDir}/src/main/generated")
-    apiPackage.set("at.museumsbahnen.api")
-    invokerPackage.set("at.museumsbahnen.invoker")
-    modelPackage.set("at.museumsbahnen.model")
-
-    additionalProperties.put("npmName", "museum-railway-client")
-    additionalProperties.put("npmVersion", version)
+    outputDir.set("${project.projectDir}/src/main/generated/typescript")
+    generatorName.set("typescript-axios")
+    configOptions.putAll(
+        mapOf(
+            "npmName" to "@museumrailwayevents/museum-railway-client",
+            "npmVersion" to "${project.version}",
+            "supportsES6" to "true",
+        )
+    )
 }
