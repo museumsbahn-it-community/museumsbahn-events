a7b500779089cb46aa09e6eec183f867d2a7a3fa
Tobias Watzl <twatzl@gmx.at>
2024-03-12 20:32:10 +0100

    fix page title styling

--- museum-railway-web/components/PageTitle.vue
+++ museum-railway-web/components/PageTitle.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-white">
     <div class="h-3rem"></div>
-    <LayoutHeaderBoxDiagonal class="w-11 lg:w-9 h-3rem">
+    <LayoutHeaderBoxDiagonal class="w-11 lg:w-9">
       <div class="h-full w-full flex align-items-center">
         <div class="w-1 lg:w-4"></div>
         <div class="w-11 lg:w-8 justify-content-center">
-          <h1 class="light-text">{{ title }}</h1>
+          <h1 class="light-text my-2">{{ title }}</h1>
         </div>
       </div>
     </LayoutHeaderBoxDiagonal>
