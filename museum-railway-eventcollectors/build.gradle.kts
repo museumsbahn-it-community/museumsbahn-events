621d289e8bb4a55b41451975dae590c386d58d48
Tobias Watzl <twatzl@gmx.at>
2024-03-31 22:59:17 +0200

    update backend dockerfile, locations, operators and dependencies for eventcollectors

--- museum-railway-eventcollectors/build.gradle.kts
+++ museum-railway-eventcollectors/build.gradle.kts
@@ -9,9 +9,9 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("events.boudicca:eventdb-openapi:0.5.0-SNAPSHOT")
-    implementation("events.boudicca:semantic-conventions:0.5.0-SNAPSHOT")
-    implementation("events.boudicca:eventcollector-client:0.5.0-SNAPSHOT")
+    implementation("events.boudicca:eventdb-openapi:0.4.1")
+    implementation("events.boudicca:semantic-conventions:0.4.1")
+    implementation("events.boudicca:eventcollector-client:0.4.1")
     implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.9.3")
     implementation("org.jsoup:jsoup:1.16.1")
     implementation("org.mnode.ical4j:ical4j:3.2.14")
