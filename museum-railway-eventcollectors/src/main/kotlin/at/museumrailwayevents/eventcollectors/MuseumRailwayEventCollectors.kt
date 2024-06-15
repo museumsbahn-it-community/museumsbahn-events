21619cab189e264caa203f57e701bee3aecc1232
Tobias Watzl <twatzl@gmx.at>
2024-06-15 17:42:52 +0200

    add new collectors to config

--- museum-railway-eventcollectors/src/main/kotlin/at/museumrailwayevents/eventcollectors/MuseumRailwayEventCollectors.kt
+++ museum-railway-eventcollectors/src/main/kotlin/at/museumrailwayevents/eventcollectors/MuseumRailwayEventCollectors.kt
@@ -1,5 +1,6 @@
 package at.museumrailwayevents.eventcollectors
 
+import AtterseeSchifffahrtCollector
 import at.museumrailwayevents.eventcollectors.collectors.*
 import at.museumrailwayevents.eventcollectors.service.JsoupCrawlerImpl
 import base.boudicca.api.eventcollector.EventCollectorCoordinatorBuilder
@@ -7,6 +8,9 @@ import base.boudicca.api.eventcollector.EventCollectorCoordinatorBuilder
 val crawler = JsoupCrawlerImpl()
 fun main() {
     EventCollectorCoordinatorBuilder()
+        .addEventCollector(AtterseebahnCollector(crawler))
+        .addEventCollector(AtterseeSchifffahrtCollector(crawler))
+        .addEventCollector(EbflCollector(crawler))
         .addEventCollector(OegegShopCollector(crawler))
         .addEventCollector(OegegSchmalspurCollector(crawler))
         .addEventCollector(ProBahnVorarlbergCollector())
@@ -16,12 +20,15 @@ fun main() {
         .addEventCollector(WackelsteinexpressCollector(crawler))
         .addEventCollector(HoellentalbahnCollector(crawler))
         .addEventCollector(YbbstalbahnCollector(crawler))
-        .addEventCollector(EbflCollector(crawler))
         .addEventCollector(Mh6Collector(crawler))
         .addEventCollector(WaldviertelbahnCollector(crawler))
         .addEventCollector(ReblausexpressCollector(crawler))
         .addEventCollector(SteirischeEisenbahnfreundeCollector(crawler))
         .addEventCollector(EbmSchwechatCollector(crawler))
+        .addEventCollector(WienerTramwayMuseumCollector(crawler))
+        .addEventCollector(TraunseetramCollector(crawler))
+        .addEventCollector(TramwaymuseumGrazCollector(crawler))
+        .addEventCollector(MLVZwettlCollector(crawler))
         .build()
         .startWebUi()
         .run()
