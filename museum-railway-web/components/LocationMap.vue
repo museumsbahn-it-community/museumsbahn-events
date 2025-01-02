f19bc6d825333a07a21ce23b1edc90502ea3d502
Tobias Watzl <twatzl@gmx.at>
2025-01-02 22:37:20 +0100

    fix leaflet map after dependency updates

--- museum-railway-web/components/LocationMap.vue
+++ museum-railway-web/components/LocationMap.vue
@@ -6,37 +6,32 @@
 }
 </style>
 <template>
-  <LMap
-      class="h-full w-full map-container"
-      ref="locationMap"
-      :center="DEFAULT_CENTER"
-      :zoom="DEFAULT_ZOOM"
-  >
-    <LTileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-        layer-type="base"
-        name="OpenStreetMap"
-    />
+  <!-- :use-global-leaflet="false" because of https://github.com/vue-leaflet/vue-leaflet/issues/278#issuecomment-1448388783 -->
+  <LMap class="h-full w-full map-container" ref="locationMap" :use-global-leaflet="false" :center="DEFAULT_CENTER"
+    :zoom="DEFAULT_ZOOM">
+    <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+      layer-type="base" name="OpenStreetMap" />
     <LMarker v-for="location in locationsWithCoords" :lat-lng="getLocationLatLng(location)">
       <LPopup>
-        <div class="flex flex-column"> 
-        <h3 class="m-1">{{location.name}}</h3>
-        <p class="m-1">
-        {{location.type}}
-        </p>
-        <div class="flex justify-content-end">
-        <Button  @click="openLocationDetails(location.locationId)" text>
-                    Details
-        </Button>
-      </div>
+        <div class="flex flex-column">
+          <h3 class="m-1">{{ location.name }}</h3>
+          <p class="m-1">
+            {{ location.type }}
+          </p>
+          <div class="flex justify-content-end">
+            <Button @click="openLocationDetails(location.locationId)" text>
+              Details
+            </Button>
+          </div>
         </div>
       </LPopup>
     </LMarker>
   </LMap>
 </template>
 <script setup lang="ts">
-import type { MuseumLocation } from '@museumrailwayevents/museum-railway-client';
+import { computed, onMounted, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const DEFAULT_CENTER = [47.609541, 13.784662];
 const DEFAULT_ZOOM = 7;
@@ -52,12 +47,12 @@ const router = useRouter();
 const locationIdParam = route?.params?.locationId;
 
 const locationsWithCoords = computed(() => props.locations.filter((loc: MuseumLocation) => {
-      let isLocation = true;
-      if (locationIdParam != undefined) {
-        isLocation = loc.locationId === locationIdParam;
-      }
-      return isLocation && loc.location.lat != null && loc.location.lon != null;
-    }))
+  let isLocation = true;
+  if (locationIdParam != undefined) {
+    isLocation = loc.locationId === locationIdParam;
+  }
+  return isLocation && loc.location.lat != null && loc.location.lon != null;
+}))
 
 watch(() => locationMap?.value?.ready, () => updateMapData());
 watch(() => props.locations, () => updateMapData());
@@ -70,7 +65,7 @@ function initMap() {
 }
 
 function openLocationDetails(locationId: string) {
-  router.push({name: 'locationDetails', params: {locationId}});
+  router.push({ name: 'locationDetails', params: { locationId } });
 }
 
 function updateMapData() {
@@ -80,7 +75,7 @@ function updateMapData() {
       location = props.locations.filter((location) => location.locationId === locationIdParam)[0];
     }
     if (location != undefined && locationMap?.value?.leafletObject != undefined) {
-      locationMap.value.leafletObject.setView({lat: location.location.lat, lng: location.location.lon}, 10);
+      locationMap.value.leafletObject.setView({ lat: location.location.lat, lng: location.location.lon }, 10);
     }
   }
 }
