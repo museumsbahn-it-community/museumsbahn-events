f737b5991535f1f1a4dea156d4eff1e743eaf8de
Tobias Watzl <twatzl@gmx.at>
2025-03-11 22:16:02 +0100

    add native image build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,12 @@ repositories {
 
 allprojects {
     group = "at.museumrailwayevents"
-    version = "0.2.0"
+    version = "0.3.0"
+}
+
+tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootBuildImage> {
+    docker {
+        host= "unix://${System.getenv("XDG_RUNTIME_DIR")}/podman/podman.sock"
+        bindHostToBuilder.set(true)
+    }
 }
