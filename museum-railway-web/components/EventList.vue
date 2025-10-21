461f51cefea7c289d065aa3d76114ee38bbf0842
Tobias Watzl <twatzl@gmx.at>
2025-10-21 22:16:24 +0200

    add chips for filters to event list and adapt styling

--- museum-railway-web/components/EventList.vue
+++ museum-railway-web/components/EventList.vue
@@ -9,7 +9,7 @@
 </style>
 <template>
   <div v-if="eventsGroupedByMonthAndDeparture.length > 0" class="flex flex-column">
-    <div v-for="eventGroup in eventsGroupedByMonthAndDeparture" class="bg-grauweiß mb-4 w-full">
+    <div v-for="eventGroup in eventsGroupedByMonthAndDeparture" class="mb-4 w-full">
       <h1 class="my-2">{{ eventGroup.label }}</h1>
       <div class="flex flex-row flex-wrap w-full justify-content-center">
         <div v-for="eventsGroupedByDeparture in eventGroup.eventGroups"
---
+++ museum-railway-web/components/ui/ColorTile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="color-tile-container">
+    <div 
+      class="color-tile" 
+      :style="{ backgroundColor: `var(${color})` }"
+    ></div>
+    <div class="color-label">{{ label }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  color: string;
+  label: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style scoped>
+.color-tile-container {
+  display: inline-block;
+  text-align: center;
+}
+
+.color-tile {
+  width: 3rem;
+  height: 3rem;
+  border: 1px solid #ccc;
+}
+
+.color-label {
+  margin-top: 0.5rem;
+  font-size: 0.875rem;
+}
+</style>
