b1631b5aff7cb48fa1394b919ac41e46e524eed9
Tobias Watzl <twatzl@gmx.at>
2026-01-10 02:49:37 +0100

    switch analytics from matomo to umami

---
+++ infrastructure/README.md
@@ -0,0 +1,12 @@
+# Infrastructure
+
+Contains the infrastructure definitions for letting museumsbahn-events.at run locally.
+
+Not for production use.
+
+## Analytics
+
+Setup 
+
+- create website
+- paste website id into nuxt config
--- museum-railway-web/pages/impressum.vue
+++ museum-railway-web/pages/impressum.vue
@@ -1,17 +1,5 @@
 <template>
   <ArticleList title="Impressum und Datenschutz" :content-collection="contentCollection" :show-date="false">
-    <template #additionalContent>
-      <!-- this card is copied from article list, but for now here is the only place we have to use it as well -->
-      <!-- if we need further usages we MUST create a new component ArticleCard for this -->
-      <Card class="article-content px-5 py-2 mb-5">
-        <template #header>
-          <h2>Analysedaten</h2>
-        </template>
-        <template #content>
-          <MatomoAnalyticsSwitch></MatomoAnalyticsSwitch>
-        </template>
-      </Card>
-    </template>
   </ArticleList>
   <div class="default-footer"></div>
 </template>
