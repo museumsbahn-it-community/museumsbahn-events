1cc13d19326de4f1e06bc6ae076b8ba959aa1c61
Tobias Watzl <twatzl@gmx.at>
2026-09-12 12:29:02 +0000

    build(deps): bump boudicca from 0.6.0 to 0.7.0

--- museum-railway-eventcollectors-base/src/main/kotlin/at/museumrailwayevents/eventcollectors/base/runner/RunnerEnricherInterface.kt
+++ museum-railway-eventcollectors-base/src/main/kotlin/at/museumrailwayevents/eventcollectors/base/runner/RunnerEnricherInterface.kt
@@ -1,5 +1,6 @@
 package base.boudicca.api.eventcollector.runner
 
+import base.boudicca.api.enricher.DefaultEnricherClient
 import base.boudicca.api.enricher.EnricherClient
 import base.boudicca.model.Event
 import io.opentelemetry.api.OpenTelemetry
@@ -13,7 +14,7 @@ fun interface RunnerEnricherInterface {
             if (enricherUrl.isNullOrBlank()) {
                 return NoopRunnerEnricher
             }
-            val enricher = EnricherClient(enricherUrl) // TODO: add otel again once boudicca published an update
+            val enricher = DefaultEnricherClient(enricherUrl) // TODO: add otel again once boudicca published an update
             return BoudiccaRunnerEnricherInterface(enricher)
         }
     }
